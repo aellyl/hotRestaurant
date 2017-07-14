4affddbd27621043e47dcfc3e9376c7c40a1afaf
Chris R <chris.rocha@>
2017-07-13 19:15:24 -0500

    created arrays

--- server.js
+++ server.js
@@ -17,25 +17,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Star Wars Characters (DATA)
 // =============================================================
-var characters = [{
-  routeName: "yoda",
-  name: "Yoda",
-  role: "Jedi Master",
-  age: 900,
-  forcePoints: 2000
-}, {
-  routeName: "darthmaul",
-  name: "Darth Maul",
-  role: "Sith Lord",
-  age: 200,
-  forcePoints: 1200
-}, {
-  routeName: "obiwankenobi",
-  name: "Obi Wan Kenobi",
-  role: "Jedi Master",
-  age: 55,
-  forcePoints: 1350
-}];
+var reservations = [];
+var waitlist = [];
 
 // Routes
 // =============================================================
