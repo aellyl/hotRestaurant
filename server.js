8de8f6acc47ba9a9045185cc1e5718ff91bb8e77
Chris R <chris.rocha@>
2017-07-13 19:12:03 -0500

    clean out file to avoid merge conflicts

--- server.js
+++ server.js
@@ -40,51 +40,28 @@ var characters = [{
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
+// Homepage
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+// /tables Get
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
 });
 
-// Get all characters
-app.get("/all", function(req, res) {
-  res.json(characters);
+// /reserve POST
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
 });
 
-// Search for Specific Character (or all characters) - provides JSON
-app.get("/api/:characters?", function(req, res) {
-  var chosen = req.params.characters;
-
-  if (chosen) {
-    console.log(chosen);
-
-    for (var i = 0; i < characters.length; i++) {
-      if (chosen === characters[i].routeName) {
-        return res.json(characters[i]);
-      }
-    }
-    return res.json(false);
-  }
-  return res.json(characters);
+// api/tables get
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
 });
 
-// Create New Characters - takes in JSON input
-app.post("/api/new", function(req, res) {
-  var newcharacter = req.body;
-  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newcharacter);
-
-  characters.push(newcharacter);
-
-  res.json(newcharacter);
+// api/waitlist GET
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
 });
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
